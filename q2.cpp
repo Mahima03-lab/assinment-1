0d2490a1d8b66dad640c0676beb3abe8e7119f42
Mahima03-lab <tumpa.greenpark@gmail.com>
2024-08-21 12:32:31 +0530

    Update q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -1,12 +1,19 @@
 #include<iostream>
 int main()
-{
+{ 
     float x,t1,t2,t3;
     std::cout<<"Enter x=";
     std::cin>>x;
-    t1=x+(1/x);
-    t2=x+(1/t1);
-    t3=x+(1/t2);
-    std::cout<<"Required result is:"<<t3;
+    if(x==0.0)
+    {
+       std::cout<<"INVALID INPUT!!";
+    }
+    else
+    {
+      t1=x+(1/x);
+      t2=x+(1/t1);
+      t3=x+(1/t2);
+      std::cout<<"Required result is:"<<t3;
+    }
     return 0;
 }
