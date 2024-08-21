8662b793f22a03f705b3a4ebf7bddc1eb3dd86ea
Mahima03-lab <tumpa.greenpark@gmail.com>
2024-08-21 12:55:57 +0530

    Update Q3.cpp

--- Q3.cpp
+++ Q3.cpp
@@ -4,7 +4,11 @@ int main()
     float x,y;
     std::cout<<"Enter y=";
     std::cin>>y;
+    x=y-20*6/9;
+    std::cout<<x<<"\n"<<"Not expected result!!";
+    std::cout<<"\nEnter y=";
+    std::cin>>y;
     x=y-20.0*6.0/9.0;
-    std::cout<<x;
+    std::cout<<"Correct x="<<x;
     return 0;
 }
